59454b0a505caeef86b60233c611611399b132a4
Aleksandr Kuznetsov <kuznetzov.kuznetsov2004@yandex.ru>
2025-09-08 01:37:38 +0300

    feat: add check on isActive for icons

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -11,39 +11,47 @@ import { TAppHeaderUIProps } from './type';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const { pathname } = useLocation();
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <>
-            <NavLink
-              to='/'
-              className={({ isActive }) =>
-                [
-                  styles.link,
-                  isActive || pathname.includes('/ingredients')
-                    ? styles.link_active
-                    : ''
-                ].join(' ')
-              }
-            >
-              <BurgerIcon type={'primary'} />
-              <p className='text text_type_main-default ml-2 mr-10'>
-                Конструктор
-              </p>
-            </NavLink>
-          </>
-          <>
-            <NavLink
-              to='/feed'
-              className={({ isActive }) =>
-                [styles.link, isActive ? styles.link_active : ''].join(' ')
-              }
-            >
-              <ListIcon type={'primary'} />
-              <p className='text text_type_main-default ml-2'>Лента заказов</p>
-            </NavLink>
-          </>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              [
+                styles.link,
+                isActive || pathname.includes('/ingredients')
+                  ? styles.link_active
+                  : ''
+              ].join(' ')
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2 mr-10'>
+                  Конструктор
+                </p>
+              </>
+            )}
+          </NavLink>
+
+          <NavLink
+            to='/feed'
+            className={({ isActive }) =>
+              [styles.link, isActive ? styles.link_active : ''].join(' ')
+            }
+          >
+            {({ isActive }) => (
+              <>
+                <ListIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2'>
+                  Лента заказов
+                </p>
+              </>
+            )}
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <NavLink to='/' className={styles.link}>
@@ -57,10 +65,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
               [styles.link, isActive ? styles.link_active : ''].join(' ')
             }
           >
-            <ProfileIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2'>
-              {userName || 'Личный кабинет'}
-            </p>
+            {({ isActive }) => (
+              <>
+                <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+                <p className='text text_type_main-default ml-2'>
+                  {userName || 'Личный кабинет'}
+                </p>
+              </>
+            )}
           </NavLink>
         </div>
       </nav>
